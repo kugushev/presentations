016329da0fa907fa2d23d742d7a6e7e999bc438a
Kugushev Aleksandr <kugushew@gmail.com>
2017-05-28 16:34:25 +0300

    add Demo project

---
+++ Demo/Demo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CSharpSyntaxTree.ParseText("");
+        }
+    }
+}
