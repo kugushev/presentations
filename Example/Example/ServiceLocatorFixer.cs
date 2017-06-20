401d11f3b542f44cc4d669285418a84ece60b2b3
Kugushev Aleksandr <kugushew@gmail.com>
2017-06-21 00:08:17 +0300

    add todo to example

--- Example/Example/ServiceLocatorFixer.cs
+++ Example/Example/ServiceLocatorFixer.cs
@@ -18,5 +18,52 @@ namespace Example
             return null;
         }
 
+        #region Fog of war
+        
+        private static string toCamelCase(TypeSyntax s)
+            => char.ToLowerInvariant(s.ToFullString()[0]) + s.ToFullString().Substring(1);
+
+        private static string toFieldName(TypeSyntax s)
+            => "_" + toCamelCase(s);
+
+        private IEnumerable<ClassDeclarationSyntax> FindClassDeclarations(SyntaxNode parent)
+        {
+            foreach (var child in parent.ChildNodes())
+            {
+                if (child is ClassDeclarationSyntax cls)
+                    yield return cls;
+                else
+                    foreach (var subcls in FindClassDeclarations(child))
+                        yield return subcls;
+            }
+        }
+
+        private bool IsServiceLocatorUsage(InvocationExpressionSyntax node)
+        {
+            if (node.Expression is MemberAccessExpressionSyntax member &&
+                member.Expression is IdentifierNameSyntax className &&
+                member.Name is GenericNameSyntax genericMethod)
+            {
+                return className.Identifier.ToString() == "ServiceLocator" &&
+                    genericMethod.Identifier.ToString() == "Resolve";
+
+            }
+            return false;
+        }
+
+        private TypeSyntax GetServiceType(InvocationExpressionSyntax node)
+        {
+            if (node.Expression is MemberAccessExpressionSyntax member &&
+                member.Expression is IdentifierNameSyntax className &&
+                member.Name is GenericNameSyntax genericMethod)
+            {
+                return genericMethod.TypeArgumentList.Arguments.Single();
+            }
+            else
+                throw new Exception("Something wrong");
+        }
+
+
+        #endregion
     }
 }
---
+++ Resources/Example/Example/SyntaxNodeEquivalenceComparer.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    class SyntaxNodeEquivalenceComparer : IEqualityComparer<SyntaxNode>
+    {
+        public bool Equals(SyntaxNode x, SyntaxNode y) => x?.IsEquivalentTo(y, false) == true;
+
+        public int GetHashCode(SyntaxNode obj) => obj?.ToString().GetHashCode() ?? 0;
+    }
+}
