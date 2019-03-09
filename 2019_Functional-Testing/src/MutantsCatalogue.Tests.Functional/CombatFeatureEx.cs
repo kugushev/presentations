76575fb5bb7b646ce49b907cd8fd46dc166afb97
Aleksandr Kugushev <kugushew@gmail.com>
2019-03-09 13:04:35 +0300

    add presentation

---
+++ 2019_Functional-Testing/src/MutantsCatalogue.Tests.Functional/CombatFeatureEx.cs
@@ -0,0 +1,36 @@
+﻿using MutantsCatalogue.Domain.Combat;
+using MutantsCatalogue.Tests.Functional.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MutantsCatalogue.Tests.Functional
+{
+    public class CombatFeatureEx : IClassFixture<TestWebApplicationFactory>
+    {
+        private readonly TestWebApplicationFactory factory;
+
+        public CombatFeatureEx(TestWebApplicationFactory factory)
+        {
+            this.factory = factory;
+        }
+
+        [Fact]
+        public async Task Combat_AnyMutant_ReturnsCopiright()
+        {
+            // arrange
+            var client = factory.CreateClient();
+
+            // act
+            var response = await client.GetAsync("api/combat");
+
+            // assert
+            response.EnsureSuccessStatusCode();
+            var result = await response.Content.ReadAsAsync<CombatResult>();
+            Assert.Equal("Aleksandr Kugushev 42", result.Copyright);
+        }
+    }
+}
---
+++ 2019_Functional-Testing/src/MutantsCatalogue.Tests.Functional/Utils/TestWebApplicationFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using MutantsCatalogue.Application;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+
+namespace MutantsCatalogue.Tests.Functional.Utils
+{
+    public class TestWebApplicationFactory : WebApplicationFactory<Startup>
+    {
+        protected override IWebHostBuilder CreateWebHostBuilder()
+        {
+            return WebHost.CreateDefaultBuilder().UseStartup<Startup>();
+        }
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            var config = new Dictionary<string, string>
+            {
+                ["Domain:CopyrightYear"] = "42"
+            };
+            builder.ConfigureAppConfiguration(b => b.AddInMemoryCollection(config));
+            base.ConfigureWebHost(builder);
+        }
+    }
+}
